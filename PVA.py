361e65661b73c0b7b70f21b7166e19b286cb7929
cwillj21 <wjohnson@pva.net>
2020-03-19 10:26:01 -0400

    - Fixed logic around dispensing continous lines and shapes that have an end point same as start point

--- PVA.py
+++ PVA.py
@@ -2660,19 +2660,21 @@ class path_gcode(inkex.Effect):
                     g += "G01" + tool_off + c([None,None,s[5][0]+depth]) + travel_speed + " ;Penetrate\n\n"
                     g += dry + 'M98 P"' + tool + '_ON.G" ;Tool On\n'
                     g += 'G04 P' + path[j].get('valve_delay') + " ;Valve Delay ms\n\n"
-                elif curve[i][1] != 'end':
+
+                if curve[i][1] != 'end':
                     start_point = si[0]
 
                 # Valve Off Distance
                 if curve[i][1] == 'end' and vod > 0:
                     g += ";Valve Off Distance: " + str(vod) + " mm\n"
                     g += "G01" + tool_on + c(new_end_point(vod,start_point,si[0])+[s[5][1]+depth]) + dispense_speed + "\n"
-
-                if lg == "G00":
                     g += "G01" + tool_off + c(si[0]+[s[5][1]+depth]) + dispense_speed + "\n"
                 else:
-                    g += "G01" + tool_on + c(si[0]+[s[5][1]+depth]) + dispense_speed + "\n"
-                    lg = 'G01'
+                    if lg == "G00":
+                        g += "G01" + tool_on + c(si[0]+[s[5][1]+depth]) + dispense_speed + "\n"
+                        lg = 'G01'
+                    else:
+                        g += "G01" + tool_on + c(si[0]+[s[5][1]+depth]) + dispense_speed + "\n"
 
             elif s[1] == 'arc':
                 r = [(s[2][0]-s[0][0]), (s[2][1]-s[0][1])]
